7b7b9e4d09875760130bd28cf7064e738bc9b7db
denisri <denis.riviere.gm@gmail.com>
2009-08-21 19:52:12 +0000

    merge sulci-private from stable to trunk
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/sulci/sulci-private/trunk@34163 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pysigraph/src/sip/learnable.h
+++ pysigraph/src/sip/learnable.h
@@ -6,29 +6,22 @@
 
 inline PyObject* pysigraphConvertFrom_sigraph_VectorLearnableP( void * a )
 {
-#if SIP_VERSION >= 0x040400
   return sipConvertFromInstance( a, sipClass_sigraph_VectorLearnable, 0 );
-#else
-  return sipMapCppToSelfSubClass( a, sipClass_sigraph_VectorLearnable );
-#endif
 }
 
 
 inline void* pysigraphConvertTo_sigraph_VectorLearnableP( PyObject * o )
 {
   int isErr = 0;
-#if SIP_VERSION >= 0x040400
   return sipConvertToInstance( o, sipClass_sigraph_VectorLearnable, 0, 0, 0, 
                                &isErr );
-#else
-  return sipForceConvertTo_sigraph_VectorLearnable( o, &isErr );
-#endif
 }
 
 
 inline int pysigraphVectorLearnableP_Check( PyObject* o )
 {
-  return sipIsSubClassInstance( o, sipClass_sigraph_VectorLearnable );
+  return sipCanConvertToInstance( o, sipClass_sigraph_VectorLearnable,
+                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 #endif
