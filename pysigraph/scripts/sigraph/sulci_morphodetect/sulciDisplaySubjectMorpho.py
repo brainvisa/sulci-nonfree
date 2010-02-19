569cd7e29b67b0ca3699971a3f0a12f2d0b8f0d7
denisri <denis.riviere.gm@gmail.com>
2010-02-19 17:19:13 +0000

    improvements in sulci morphometry tools
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/sulci/sulci-private/trunk@36760 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ pysigraph/scripts/sigraph/sulci_morphodetect/sulciDisplaySubjectMorpho.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import anatomist.direct.api as ana
+from soma import aims
+import os, sys,numpy
+
+def loadMorpho( wdir ):
+  morpho = {}
+  files = os.listdir( wdir )
+  files.sort()
+  for fn in files:
+    if fn.endswith( '.dat' ):
+      fn = os.path.join( wdir, fn )
+      f = open( fn )
+      hdr = f.readline().strip().split()
+      l1 = f.readline().split()
+      f.close()
+      ncols = len( hdr )
+      label = l1[1]
+      side = l1[2]
+      label += '_' + side
+      n = numpy.loadtxt( fn, skiprows=1, usecols=range(3,ncols) ).reshape( ( -1, ncols-3 ) )
+      subjects = numpy.loadtxt( fn, skiprows=1, usecols=[0], dtype=str ).reshape( ( n.shape[0], ) )
+      morpho[ label ] = [ hdr[3:], n, subjects ]
+  return morpho
+
+def loadstat( file ):
+  f = open( file )
+  hdr = f.readline().strip().split()
+  f.close()
+  ncols = len( hdr )
+  mean = numpy.loadtxt( file, skiprows=1, usecols=range(2,ncols) )
+  meant = {}
+  f = open( file )
+  f.readline()
+  i = 0
+  for l in f.xreadlines():
+    l = l.strip().split()
+    label = l[0]
+    side = l[1]
+    label += '_' + side
+    meant[ label ] = mean[i, :]
+    i += 1
+  return meant
+
+def mapProperty( graph, statprop, prop, subject, labelprop, morpho, means, std ):
+  vert = graph.vertices()
+  for v in vert:
+    if v.has_key( labelprop ):
+      label = v[ labelprop ]
+      measures = morpho.get( label )
+      if measures:
+        col = measures[0].index( prop )
+        line = numpy.where( measures[2] == subject )[0][0]
+        meas = measures[1][ line ][col]
+        s = stds[label][col]
+        if s == 0:
+          meas = 0
+        else:
+          meas = (meas - means[label][col] ) / s
+        v[ statprop ] = meas
+
+if __name__ == '__main__':
+  if sys.argv[1] in ( '-h', '--help' ):
+    print 'usage:'
+    print sys.argv[0], 'morphomean.dat morphostd.dat subject.arg morphodir property subjectname'
+    print ''
+    sys.exit(0)
+
+  morpho = loadMorpho( sys.argv[4] )
+  means = loadstat( sys.argv[1] )
+  stds = loadstat( sys.argv[2] )
+  graph = aims.read( sys.argv[3] )
+  prop = sys.argv[5]
+  subject = sys.argv[6]
+  mapProperty( graph, 'stat', prop, subject, 'label', morpho,
+    means, stds )
+
+a = ana.Anatomist()
+ag = ana.cpp.AObjectConverter.anatomist( graph )
+a.execute( 'GraphDisplayProperties', objects=[ag], display_mode='PropertyMap',
+  display_property='stat' )
+w = a.createWindow( '3D' )
+a.execute( 'SetObjectPalette', objects=[ag], absoluteMode=1, min=-3., max=3. )
+w.addObjects( [ag], add_graph_nodes=True )
+
+if sys.modules.has_key( 'PyQt4' ):
+  from PyQt4 import QtGui
+  if QtGui.qApp.startingUp():
+    QtGui.qApp.exec_()
+else:
+  import qt
+  if qt.qApp.startingUp():
+    qt.qApp.exec_loop()
+
--- pysigraph/scripts/sigraph/sulci_morphodetect/sulciRecordStats_bv.py
+++ pysigraph/scripts/sigraph/sulci_morphodetect/sulciRecordStats_bv.py
@@ -1,9 +1,11 @@
 
 from neuroProcesses import *
+import neuroConfig
+import sys, os
 
 signature = Signature(
   'input_graphs', ListOf( ReadDiskItem( 'Cortical Folds Graph', 'Graph and data' ) ),
-  'model_graph', ReadDiskItem( 'Model Graph', 'Model Graph' ),
+  'model_graph', ReadDiskItem( 'Model Graph', 'Graph' ),
   'output_stats', WriteDiskItem( 'Text File', 'Text File' ),
 )
 
@@ -11,7 +13,14 @@ def initialization( self ):
   pass
 
 def execution( self, context ):
-  import sulciRecordStats
+  pth = os.path.join( neuroConfig.mainPath, 'scripts', 'sigraph',
+    'sulci_morphodetect' )
+  cleanpath = False
+  if pth not in sys.path:
+    sys.path.insert( 0, pth )
+    cleanpath = True
+  from sulciRecordStats import sulciRecordStats
   sulciRecordStats( [ x.fullPath() for x in self.input_graphs ],
     self.model_graph.fullPath(), self.output_stats.fullPath() )
-
+  if cleanpath:
+    del sys.path[0]
