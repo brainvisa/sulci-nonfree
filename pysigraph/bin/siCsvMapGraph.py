e8b6f75102becbf66b059d0f6473099a1b4f0998
denisri <denis.riviere.gm@gmail.com>
2010-08-17 21:24:24 +0000

    Adapted siCsvMapGraph to a wider range of csv files organization, with subject/label columns at random positions. Tried to add an optional column selection option, but with no luck so far...
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/sulci/sulci-private/branches/4.0@38632 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pysigraph/bin/siCsvMapGraph.py
+++ pysigraph/bin/siCsvMapGraph.py
@@ -47,26 +47,45 @@ def print_csv_format():
 	'''
 	print format
 
-def read_csv(csvfilename):
+def read_csv(csvfilename, column=None):
 	fd = open(csvfilename)
 	lines = fd.readlines()
-	labels = lines[0].rstrip('\n').split('\t')
+	delim = '\t'
+	labels = lines[0].rstrip('\n').rstrip('\r').split( delim )
+	if len( labels ) == 1:
+	  delim = ' '
+          labels = lines[0].rstrip('\n').rstrip('\r').split( delim )
 	fd.close()
 	header_minf = { 'Y' : [], 'labels' : labels }
-	if labels[0] == 'subjects' and labels[1] in ['sulci', 'nodes']:
-		header_minf['X'] = range(2, len(labels))
-		header_minf['INF'] = [0, 1]
-		mode = labels[1]
-		labels = labels[2:]
-	elif labels[0] in ['sulci', 'nodes']:
-		header_minf['X'] = range(1, len(labels))
-		header_minf['INF'] = [0]
-		mode = labels[0]
-		labels = labels[1:]
+	labels2 = [ x.lower() for x in labels ]
+	subjectcol = None
+	labelcol = None
+	for sl in ( 'subjects', 'subject' ):
+	  if sl in labels2:
+	    subjectcol = labels2.index( sl )
+	    break
+	for sl in ( 'sulci', 'nodes', 'label' ):
+	  if sl in labels2:
+	    labelcol = labels2.index( sl )
+	if subjectcol is not None and labelcol is not None:
+		header_minf['X'] = range(labelcol+1, len(labels))
+		header_minf['INF'] = [subjectcol, labelcol]
+		mode = labels2[labelcol]
+		olabels = labels[labelcol+1:]
+	elif subjectcol is not None:
+		header_minf['X'] = range(labelcol+1, len(labels))
+		header_minf['INF'] = [labelcol]
+		mode = labels2[labelcol]
+		olabels = labels[labelcol+1:]
 	else:
 		print "bad csv format"
 		sys.exit(1)
-	db, header = io.ReaderHeaderCsv().read(csvfilename,header_minf)
+	if column is not None:
+	  header_minf['X'] = [ column ]
+	  header_minf['INF'] = range( column ) + range( column+1, len(labels) )
+	  olabels = [ labels[ column ] ]
+	db, header = io.ReaderHeaderCsv().read(csvfilename,header_minf, 
+	  sep=delim)
 	X = db.getX()
 	sulci = db.getINF()[:, -1]
 	uniq_sulci = numpy.unique1d(sulci)
@@ -76,6 +95,8 @@ def read_csv(csvfilename):
 		X2m = X2.mean(axis=0)
 		X2s = X2.std(axis=0)
 		X2sum = X2.sum(axis=0)
+		if s.startswith( "'" ) and s.endswith( "'" ):
+		  s = s[1:-1]
 		sulci_data[s] = X2m, X2s, X2sum
 	X3 = numpy.vstack([data[0] for data in sulci_data.values()])
 	X4 = numpy.vstack([data[2] for data in sulci_data.values()])
@@ -85,7 +106,7 @@ def read_csv(csvfilename):
 	print "global mean over sulci :", Xm
 	print "global std over sulci : ", Xs
 	print "global sum over sulci :", Xsum
-	return labels, sulci_data, mode
+	return olabels, sulci_data, mode
 
 def write_summary_csv(csvfilename, labels, sulci_data, mode):
 	from datamind.ml.database import DbNumpy
@@ -128,12 +149,28 @@ def parseOpts(argv):
 		help='print csv format')
 	parser.add_option('-t', '--translation', dest='transfile',
 		metavar = 'FILE', action='store', default = transfile,
-		help='translation file (default : %default)')
+		help='translation file (.trl), or nomenclature file (.hie), ' \
+		'or selection file (.sel) (default : %default)')
 	parser.add_option('--log', dest='log',
 		metavar = 'FILE', action='store_true', default=False,
 		help='add log of mean values read in the input csv')
+	# parser.add_option('-c', '--column', dest='column', default=None,
+	#         type='int', help='column number to be used in the csv file')
 	return parser, parser.parse_args(argv)
 
+def getdata( sulci_data, label ):
+  try:
+    data = sulci_data[ label ]
+    return data
+  except:
+    if label.endswith( '_left' ):
+      label = label[:-5]
+    elif label.endswith( '_right' ):
+      label = label[:-6]
+    else:
+      raise
+    data = sulci_data[ label ]
+    return data
 
 def main():
 	parser, (options, args) = parseOpts(sys.argv)
@@ -146,7 +183,8 @@ def main():
 	# read
 	ft = sigraph.FoldLabelsTranslator(options.transfile)
 	sigraph.si().setLabelsTranslPath(options.transfile)
-	labels, sulci_data, mode = read_csv(options.csvfilename)
+	labels, sulci_data, mode = read_csv(options.csvfilename ) #, 
+	  # options.column)
 	if options.summarycsvfilename:
 		write_summary_csv(options.summarycsvfilename,
 					labels, sulci_data, mode)
@@ -156,7 +194,7 @@ def main():
 	from soma import aims
 	r = aims.Reader(options = {'subobjectsfilter' : 1})
 	g = r.read(options.graphname)
-	ft.translate(g)
+	ft.translate(g, options.label_attribute, options.label_attribute)
 
 	# mesh
 	if options.meshname:
@@ -165,11 +203,15 @@ def main():
 
 	for v in g.vertices():
 		if v.getSyntax() != 'fold': continue
-		if mode == 'sulci':
-			try: data = sulci_data[v[options.label_attribute]]
-			except exceptions.KeyError: continue
+		if mode in ( 'sulci', 'label' ):
+			try: 
+			  l = v[options.label_attribute]
+			  data = getdata( sulci_data, 
+			    v[options.label_attribute] )
+			except exceptions.KeyError: 
+			  continue
 		elif mode == 'nodes':
-			try: data = sulci_data[str(int(v['index']))]
+			try: data = getdata( sulci_data, str(int(v['index'])) )
 			except exceptions.KeyError: continue
 		for i, h in enumerate(labels):
 			v['csv_mean_' + h] = data[0][i]
@@ -179,6 +221,7 @@ def main():
 			if data[1][i]: v['csv_std_' + h] = data[1][i]
 			v['csv_sum_' + h] = data[2][i]
 
+	app = qt.QApplication( sys.argv )
 	a = anatomist.Anatomist()
 	ag = a.toAObject(g)
 	ag.setColorMode(ag.PropertyMap)
