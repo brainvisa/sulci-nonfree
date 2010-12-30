05a52855776c2d463363f46b2ec9ad0e6c5a5f4b
denisri <denis.riviere.gm@gmail.com>
2010-12-30 00:23:09 +0000

    added an option -a / --append to decide whether error measurements should be appended at the end of a file or replace them. Warning: the default is to replace, contrarily of the former behaviour
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/sulci/sulci-private/trunk@40384 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pysigraph/bin/siErrorLightWrapper.py
+++ pysigraph/bin/siErrorLightWrapper.py
@@ -7,51 +7,59 @@ from sulci.common import lock, unlock
 from soma.sorted_dictionary import SortedDictionary
 
 
-def addInfoToCSV(csvfilename, graphname, errors, nrj):
-	lock(csvfilename + '.lock')
-	fd = open(csvfilename, 'a')
-	if fd.tell() == 0:
-		h = 'Subject\t%s\tEnergy\n' % '\t'.join(errors.keys())
-		fd.write(h)
-	errors_str = '\t'.join(str(x) for x in errors.values())
-	fd.write('%s\t%s\t%s\n' % (graphname, errors_str, str(nrj)))
-	fd.close()
-	unlock(csvfilename + '.lock')
+def addInfoToCSV(csvfilename, graphname, errors, nrj, append=False):
+    lock(csvfilename + '.lock')
+    if append:
+        mode = 'a'
+    else:
+        mode = 'w'
+    fd = open(csvfilename, mode)
+    if fd.tell() == 0:
+        h = 'Subject\t%s\tEnergy\n' % '\t'.join(errors.keys())
+        fd.write(h)
+    errors_str = '\t'.join(str(x) for x in errors.values())
+    fd.write('%s\t%s\t%s\n' % (graphname, errors_str, str(nrj)))
+    fd.close()
+    unlock(csvfilename + '.lock')
 
 
 
 # Options parser
 def parseOpts(argv):
-	description = 'Call siError on graph inputs and store resulting ' \
-			'error rate in a new entry of a CSV file.'
-	parser = OptionParser(description)
-	parser.add_option('-m', '--model', action='store',
-		dest='model', metavar = 'FILE', default = None,
-		help='graph model')
-	parser.add_option('-l', '--labeled_graph', action='store',
-		dest='labeled_graph', metavar = 'FILE', default = None,
-		help='labeled graph file (evaluated one)')
-	transfile = os.path.join(aims.carto.Paths.shfjShared(),
-		'nomenclature', 'translation', 'sulci_model_noroots.trl')
-	parser.add_option('-t', '--labels_translation', action='store',
-		dest='labels_translation', metavar = 'FILE',
-		default = transfile, help='labels translation file')
-	parser.add_option('-b', '--base_graph', action='store',
-		dest='base_graph', metavar = 'FILE', default = None,
-		help='base graph file (reference) If not specified, '
-		'labeled_graph is taken')
-	parser.add_option('-c', '--csv', action='store',
-		dest='csvfilename', metavar = 'CSV', default = None,
-		help='file storing error rate')
-	parser.add_option('-n', '--graph_name', action='store',
-		dest='graphname', metavar = 'NAME', default = None,
-		help='graph name to be stored in CSV')
-	options, args = parser.parse_args(argv)
-	if None in [options.labeled_graph,
-			options.csvfilename, options.graphname]:
-		parser.print_help()
-		sys.exit(1)
-	return options, args
+    description = 'Call siError on graph inputs and store resulting ' \
+                  'error rate in a new entry of a CSV file.'
+    parser = OptionParser(description)
+    parser.add_option('-m', '--model', action='store',
+        dest='model', metavar = 'FILE', default = None,
+        help='graph model')
+    parser.add_option('-l', '--labeled_graph', action='store',
+        dest='labeled_graph', metavar = 'FILE', default = None,
+        help='labeled graph file (evaluated one)')
+    transfile = os.path.join(aims.carto.Paths.shfjShared(),
+        'nomenclature', 'translation', 'sulci_model_noroots.trl')
+    parser.add_option('-t', '--labels_translation', action='store',
+        dest='labels_translation', metavar = 'FILE',
+        default = transfile, help='labels translation file')
+    parser.add_option('-b', '--base_graph', action='store',
+        dest='base_graph', metavar = 'FILE', default = None,
+        help='base graph file (reference) If not specified, '
+        'labeled_graph is taken')
+    parser.add_option('-c', '--csv', action='store',
+        dest='csvfilename', metavar = 'CSV', default = None,
+        help='file storing error rate')
+    parser.add_option('-n', '--graph_name', action='store',
+        dest='graphname', metavar = 'NAME', default = None,
+        help='graph name to be stored in CSV')
+    parser.add_option('-a', '--append', action='store_true',
+        dest='append', default = False,
+        help='append errors at the end of an existing files instead of ' \
+        'erasing it')
+    options, args = parser.parse_args(argv)
+    if None in [options.labeled_graph, options.csvfilename,
+        options.graphname]:
+        parser.print_help()
+        sys.exit(1)
+    return options, args
 
 
 
@@ -76,8 +84,7 @@ def main():
 		print "Energy = %f %%" % nrj
 	else:	nrj = numpy.nan
 	addInfoToCSV(options.csvfilename, options.graphname,
-					global_errors2, nrj)
-		
+                     global_errors2, nrj, append=options.append)
 
 
 if __name__ == "__main__" : main()
