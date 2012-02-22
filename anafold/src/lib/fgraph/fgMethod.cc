cdc228b0ad934bdbff380f776a4c79a5be3c9aa6
clarafischer <clarafischer@users.noreply.github.com>
2012-02-22 17:36:36 +0000

    moving anafold to sulci-private
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/sulci-private/trunk@47646 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ anafold/src/lib/fgraph/fgMethod.cc
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (C) 2000-2004 CEA
+ *
+ *  This software and supporting documentation were developed by
+ *  	CEA/DSV/SHFJ
+ *  	4 place du General Leclerc
+ *  	91401 Orsay cedex
+ *  	France
+ *
+ */
+
+#include <anafold/fgraph/fgMethod.h>
+#include <anafold/fgraph/afgraph.h>
+#include <si/graph/mgraph.h>
+#include <si/graph/cgraph.h>
+#include <qobject.h>
+
+using namespace anatomist;
+using namespace sigraph;
+using namespace std;
+
+
+string FGraphMethod::ID() const
+{
+  return( QT_TRANSLATE_NOOP( "FusionChooser", "FusionGraphs" ) );
+}
+
+
+FGraphMethod::~FGraphMethod()
+{
+}
+
+
+int FGraphMethod::canFusion( const set<AObject *> & obj )
+{
+  if( obj.size() != 2 )
+    return 0;
+
+  set<AObject *>::const_iterator	io = obj.begin();
+  AObject				*o1, *o2;
+
+  o1 = *io;
+  ++io;
+  o2 = *io;
+
+  if( o1->type() == AObject::GRAPH && o2->type() == AObject::GRAPH )
+  {
+    if( dynamic_cast<MGraph *>( ((AGraph *) o1)->graph() ) )
+    {
+      if( dynamic_cast<CGraph *>( ((AGraph *) o2)->graph() ) )
+        return 170;
+      else
+        return 0;
+    }
+    else if( dynamic_cast<CGraph *>( ((AGraph *) o1)->graph() ) )
+    {
+      if( dynamic_cast<MGraph *>( ((AGraph *) o2)->graph() ) )
+        return 150;
+      else
+        return 0;
+    }
+  }
+
+  return 0;
+}
+
+
+AObject* FGraphMethod::fusion( const vector<AObject *> & obj )
+{
+  vector<AObject *>::const_iterator	io = obj.begin();
+  AGraph				*o = (AGraph *) *io;
+
+  ++io;
+  return( new AFGraph( o, (AGraph *) *io ) );
+}
+
+
