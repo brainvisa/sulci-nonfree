3c05d78e5329a88edd3c7deb2e5b45dfaaefe725
denisri <denis.riviere.gm@gmail.com>
2012-11-05 15:42:14 +0000

    command to check consistency of manual sulci labelings. Currently, just checks labels side.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/sulci-private/trunk@51853 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ pysigraph/bin/sulciLabelConsistencyCheck.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import optparse
+from soma import aims
+import sys
+
+# --- args
+parser = optparse.OptionParser(
+    description='Consistency check for sulci graph labels: typically, ensure there is no hemisphere mismatch (left label in right hemisphere).' )
+parser.add_option( '-i', '--input', dest='graph', help='labelled sulci graph' )
+parser.add_option( '-l', '--labelatt', dest='label', default=None,
+    help='label attribute (label or name), default: guessed if specified in ' \
+    'graph, or take name' )
+parser.add_option( '-s', '--side', dest='side', default=None,
+    help='side (left or right)' )
+
+(options, args) = parser.parse_args()
+
+graphfile = options.graph
+labelatt = options.label
+side = options.side
+
+if not graphfile:
+    parser.print_help()
+    sys.exit( 2 )
+
+graph = aims.read( graphfile )
+
+if not labelatt:
+    try:
+        print 'checking label_property'
+        labelatt = graph[ 'label_property' ]
+        print 'done'
+        print labelatt
+    except:
+        pass
+    if not labelatt:
+        print >> sys.stderr, 'Could not guess label attribute.'
+        sys.exit( 2 )
+
+leftlabels = set()
+rightlabels = set()
+
+for v in graph.vertices():
+    label = v[ labelatt ]
+    if label.endswith( '_left' ):
+        leftlabels.add( label )
+    elif label.endswith( '_right' ):
+        rightlabels.add( label )
+
+print 'left labels:', len( leftlabels )
+print 'right labels:', len( rightlabels )
+
+if side is not None:
+    if side == 'left':
+        if len( rightlabels ) != 0:
+            print >> sys.stderr, \
+                '** inconsistency: %d right labels present **' \
+                % len( rightlabels )
+            sys.exit( 1 )
+        else:
+            print 'No inconsistency found.'
+            sys.exit( 0 )
+    elif side == 'right':
+        if len( leftlabels ) != 0:
+            print >> sys.stderr, \
+                '** inconsistency: %d left labels present **' \
+                % len( leftlabels )
+            sys.exit( 1 )
+        else:
+            print 'No inconsistency found.'
+            sys.exit( 0 )
+    else:
+        print >> sys.stderr, 'cannot understand side parameter - ignoring'
+
+if len( leftlabels ) != 0 and len( rightlabels ) != 0:
+    print >> sys.stderr, \
+        '** inconsistency: %d left labels and %d right labels present **' \
+        % ( len( leftlabels ), len( rightlabels ) )
+    sys.exit( 1 )
+else:
+    print 'No inconsistency found.'
+
