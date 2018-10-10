7a5fc94a256bb6ceb234176f965d8bc7f423846b
denisri <denis.riviere.gm@gmail.com>
2018-10-10 09:03:25 +0000

    other fixes in parameters
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/sulci-private/branches/bug_fix@83512 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pysigraph/python/sulci/capsul/spam_test_labeling_local_for_loo.py
+++ pysigraph/python/sulci/capsul/spam_test_labeling_local_for_loo.py
@@ -37,14 +37,14 @@ class SpamTestLabelingLocalForLOO(Process):
         local_referentials = os.path.join(self.local_referentials_directory,
                                           self.loo_subject + '.dat')
         direction_priors = os.path.join(self.transformation_priors_directory,
-                                        'bingham_direction_trm_priors',
-                                        self.loo_subject + '.dat')
+                                        self.loo_subject,
+                                        'bingham_direction_trm_priors.dat')
         angle_priors = os.path.join(self.transformation_priors_directory,
-                                    'vonmises_angle_trm_priors',
-                                    self.loo_subject + '.dat')
-        translation_priors = os.path.join(self.transformation_priors_directory,
-                                          'gaussian_translation_trm_priors',
-                                          self.loo_subject + '.dat')
+                                    self.loo_subject,
+                                    'vonmises_angle_trm_priors.dat')
+        translation_priors = os.path.join(
+            self.transformation_priors_directory, self.loo_subject,
+            'gaussian_translation_trm_priors.dat')
 
         odir = os.path.join(self.output_test_directory, self.loo_subject)
         output_graph = os.path.join(odir, self.loo_subject + '_local.arg')
