35e1582f78fe679ccc268a95d58c65c6fa1581de
denisri <denis.riviere.gm@gmail.com>
2019-03-07 09:44:19 +0000

    adapt error averaging process to new pipeline
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/morphologist/sulci-private/branches/bug_fix@84567 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pysigraph/python/sulci/capsul/sulci_labeling_avg_error.py
+++ pysigraph/python/sulci/capsul/sulci_labeling_avg_error.py
@@ -10,84 +10,83 @@ class SulciLabelingAvgError(Process):
 
     error_dir = traits.List(traits.Directory(output=False))
     output_file = traits.File(output=False, allowed_extensions=['.csv'])
+    patterns = traits.List(traits.Str(), ['*/*_err.csv'])
 
     def _run_process(self):
         stats = []
         bylabel = []
         bysubject = []
         for d in self.error_dir:
-            #tsum = 0.
-            #weight = 0.
-            sbylabel = {}
-            wbylabel = {}
-            sbysubject = {}
-            wbysubject = {}
-            for sd in os.listdir(d):
-                fd = os.path.join(d, sd)
-                if os.path.isdir(fd):
-                    for f in os.listdir(fd):
-                        if f.endswith('_err.csv'):
-                            ef = os.path.join(fd, f)
-                            print('read:', ef)
-                            with open(ef) as ief:
-                                hdr = ief.readline().strip().split()
-                                SI = hdr.index('SI_error')
-                                TP = hdr.index('true_positive')
-                                FP = hdr.index('false_positive')
-                                FN = hdr.index('false_negative')
-                                sj = hdr.index('subjects')
-                                sc = hdr.index('sulci')
-                                while ief:
-                                    l = ief.readline().strip().split()
-                                    if l:
-                                        w = float(l[FN]) + float(l[TP])
-                                        s = float(l[SI]) * w
-                                        subject = l[sj]
-                                        sulcus = l[sc]
-                                        if sulcus != 'unknown' \
-                                                and not sulcus.startswith(
-                                                    'ventricle'):
-                                            #tsum += s
-                                            #weight += w
-                                            wbysubject[subject] \
-                                                = wbysubject.setdefault(
-                                                    subject, 0.) + w
-                                            sbysubject[subject] \
-                                                = sbysubject.setdefault(
-                                                    subject, 0.) + s
-                                        wbylabel[sulcus] \
-                                            = wbylabel.setdefault(sulcus, 0.) \
-                                                + w
-                                        sbylabel[sulcus] \
-                                            = sbylabel.setdefault(sulcus, 0.) \
-                                                + s
-                                    else:
-                                        break
-            #if weight != 0.:
-                #avg = tsum / weight
-            #else:
-                #avg = 0.
-            #stats.append((d, avg))
-            for subject in sorted(sbysubject.keys()):
-                s = sbysubject[subject]
-                w = wbysubject[subject]
-                err = 0.
-                if w != 0.:
-                    err = s / w
-                bysubject.append((d, subject, err))
-            if len(bysubject) != 0.:
-                avg = sum([y for x, s, y in bysubject if x == d]) \
-                    / len(sbysubject)
-            else:
-                avg = 0.
-            stats.append((d, avg))
-            for sulcus in sorted(sbylabel.keys()):
-                s = sbylabel[sulcus]
-                w = wbylabel[sulcus]
-                err = 0.
-                if w != 0.:
-                    err = s / w
-                bylabel.append((d, sulcus, err))
+            for pattern in self.patterns:
+                #tsum = 0.
+                #weight = 0.
+                case = os.path.join(d, pattern)
+                sbylabel = {}
+                wbylabel = {}
+                sbysubject = {}
+                wbysubject = {}
+                err_files = glob.glob(os.path.join(d, pattern))
+                for ef in err_files:
+                    print('read:', ef)
+                    with open(ef) as ief:
+                        hdr = ief.readline().strip().split()
+                        SI = hdr.index('SI_error')
+                        TP = hdr.index('true_positive')
+                        FP = hdr.index('false_positive')
+                        FN = hdr.index('false_negative')
+                        sj = hdr.index('subjects')
+                        sc = hdr.index('sulci')
+                        while ief:
+                            l = ief.readline().strip().split()
+                            if l:
+                                w = float(l[FN]) + float(l[TP])
+                                s = float(l[SI]) * w
+                                subject = l[sj]
+                                sulcus = l[sc]
+                                if sulcus != 'unknown' \
+                                        and not sulcus.startswith(
+                                            'ventricle'):
+                                    #tsum += s
+                                    #weight += w
+                                    wbysubject[subject] \
+                                        = wbysubject.setdefault(
+                                            subject, 0.) + w
+                                    sbysubject[subject] \
+                                        = sbysubject.setdefault(
+                                            subject, 0.) + s
+                                wbylabel[sulcus] \
+                                    = wbylabel.setdefault(sulcus, 0.) \
+                                        + w
+                                sbylabel[sulcus] \
+                                    = sbylabel.setdefault(sulcus, 0.) \
+                                        + s
+                            else:
+                                break
+                #if weight != 0.:
+                    #avg = tsum / weight
+                #else:
+                    #avg = 0.
+                #stats.append((case, avg))
+                for subject in sorted(sbysubject.keys()):
+                    s = sbysubject[subject]
+                    w = wbysubject[subject]
+                    err = 0.
+                    if w != 0.:
+                        err = s / w
+                    bysubject.append((case, subject, err))
+                if len(bysubject) != 0.:
+                    avg = sum([y for x, s, y in bysubject if x == case]) \
+                        / len(sbysubject)
+                else:
+                    avg = 0.
+                stats.append((case, avg))
+                for sulcus in sorted(sbylabel.keys()):
+                    s = sbylabel[sulcus]
+                    w = wbylabel[sulcus]
+                    err = 0.
+                    if w != 0.:
+                        err = s / w
+                    bylabel.append((case, sulcus, err))
 
         with open(self.output_file, 'w') as wf:
             print('case,       subject,    sulcus,     Avg.SI', file=wf)
