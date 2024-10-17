934c88222d462581f04a23f6690e242e3d4dc2ea
Denis Rivière <denis.riviere.gm@gmail.com>
2024-10-17 18:44:25 +0200

    adapt to python3

--- pysigraph/src/si_libsvm/si_libsvm.c
+++ pysigraph/src/si_libsvm/si_libsvm.c
@@ -2,21 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static PyObject *init_wrapper(PyObject *self, PyObject *null) {
-	(void)(self); /* compilation warning... */
-	(void)(null);
-	printf("init libsvm python\n");
-	return NULL;
+static PyObject *init_wrapper(PyObject *self, PyObject *null)
+{
+  (void)(self); /* compilation warning... */
+  (void)(null);
+  printf("init libsvm python\n");
+  return NULL;
 }
 
 
 static PyMethodDef methods[] = {
-	{"init", init_wrapper, METH_NOARGS, "init in python"},
-	{NULL, NULL, 0, NULL}
+  {"init", init_wrapper, METH_NOARGS, "init in python"},
+  {NULL, NULL, 0, NULL}
 };
 
 
-void	init_si_libsvm(void)
+static struct PyModuleDef cModSiSvm =
 {
-	Py_InitModule("_si_libsvm", methods);
+  PyModuleDef_HEAD_INIT,
+  "_si_svm", /* name of module */
+  "",          /* module documentation, may be NULL */
+  -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+  methods
+};
+
+
+PyMODINIT_FUNC PyInit__si_libsvm(void)
+{
+  return PyModule_Create(&cModSiSvm);
 }
